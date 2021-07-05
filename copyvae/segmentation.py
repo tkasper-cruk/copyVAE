4a5802612b854eab894f737324d28fac3748f0f1
leopardShark <lamicccc@gmail.com>
2021-07-05 13:15:41 +0200

    PELT for single sample

--- copyvae/segmentation.py
+++ copyvae/segmentation.py
@@ -1,7 +1,84 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import scipy.special as sc
 from copyvae.graphics import draw_heatmap
+
+
+def log_likelihood(x_array, lam):
+
+    n = len(x_array)
+    log_gamma = sc.gammaln(x_array + 1)
+    ll = - n * lam + np.log(lam) * np.sum(x_array) - np.sum(log_gamma)
+
+    return ll
+
+
+def cost_func(x_array):
+    """ Loss Function: -2 x LL """
+    cost = 0
+    for lam in range(1,7):
+        cost = cost + -2 * log_likelihood(x_array, lam)
+
+    return cost
+
+
+def opt_partition(bincp_array, beta):
+    """ Optimal Partitioning """
+
+    f = - beta
+    f_dict = {0:f}
+    breakpoints = []
+    n = len(bincp_array)
+
+    for j in range(1, n+1):
+        local_f = {}
+
+        for i in range(j):
+            v = f_dict[i] + cost_func(bincp_array[i+1:j]) + beta
+            local_f[i] = v
+        f_dict[j] = min(local_f.values())
+        bp = min(local_f, key=local_f.get)
+        breakpoints.append(bp)
+    breakpoints = sorted(list(set(breakpoints)))
+
+    return breakpoints
+
+
+
+def pelt_algorithm(bincp_array, beta, k=0):
+    """ PELT Method by Killick et al"""
+
+    f = - beta
+    f_dict = {0:f}
+    breakpoints = []
+    n = len(bincp_array)
+    r_set = [0]
+
+    for j in range(1, n+1):
+        local_f = {}
+
+        for i in r_set:
+            v = f_dict[i] + cost_func(bincp_array[i+1:j]) + beta
+            local_f[i] = v
+
+        f_dict[j] = min(local_f.values())
+        bp = min(local_f, key=local_f.get)
+        breakpoints.append(bp)
+
+        ## update R{}
+        r_set.append(j)
+        new_r_set = []
+        for t in r_set:
+            left = f_dict[t] + cost_func(bincp_array[t+1:j]) + k
+            right = f_dict[j]
+            if left <= right:
+                new_r_set.append(t)
+        r_set = new_r_set
+
+    breakpoints = sorted(list(set(breakpoints)))
+    return breakpoints
+
 
 def incremental_mean(x, mu_pre, n):
 
@@ -78,13 +155,27 @@ def merge_segments(bincp_array, break_loss=.01):
 
 
 
-bincp_array = np.load("median_cp.npy")
+import time
+
+bincp_array = np.load("../data/median_cp.npy")
+"""
 resli = []
 
 for row in bincp_array:
     cell_array = row
-    result = merge_segments(cell_array, .005)
+    result = merge_segments(cell_array, .1)
     resli.append(result)
 
 res = np.stack(resli, axis=0)
-draw_heatmap(res,"segment.005")
+draw_heatmap(res,"segment.1")
+"""
+cell = bincp_array[700]
+t0 = time.time()
+bps0 = opt_partition(cell, beta=25)
+t1 = time.time()
+bps1 = pelt_algorithm(cell, beta=25)
+t2 = time.time()
+print(t1-t0)
+print(t2-t1)
+print(bps0)
+print(bps1)
