8318d31458fa23bbe1eeca5c316795c38c098207
leopardShark <lamicccc@gmail.com>
2021-08-04 11:29:51 +0200

    Update cell_tools.py

--- copyvae/cell_tools.py
+++ copyvae/cell_tools.py
@@ -2,20 +2,45 @@
 
 import numpy as np
 from scipy import stats
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
+from copyvae.segmentation import pelt_multi
 
 
 @dataclass
 class Clone:
     """ Class for cell clones """
-
     id: int
     clone_size: int
     bin_size: int
     cell_gene_cn: np.ndarray = np.array([])
     cell_bin_cn: np.ndarray = np.array([])
+    chrom_bound: List[int] = field(default_factory=list)
     breakpoints: np.ndarray = np.array([])
     segment_cn: np.ndarray = np.array([])
+
+
+    def call_breakpoints(self):
+        """ Detect clone breakpoints """
+
+        k = self.clone_size
+        n = np.shape(self.cell_bin_cn)[1]
+        beta = 0.45 * k * np.log(n)
+        bp_arr = np.array([])
+
+        for tup in self.chrom_bound:
+            start_bin = tup[0]
+            end_bin = tup[1]
+            chrom_bps = pelt_multi(
+                                    self.cell_bin_cn[:, start_bin:end_bin], 
+                                    beta
+                                    )
+            bps = chrom_bps + start_bin
+            bp_arr = np.concatenate((bp_arr, bps))
+
+        self.breakpoints = bp_arr.astype(int)
+
+
 
     def generate_profile(self):
         """ Generate clone profile """
