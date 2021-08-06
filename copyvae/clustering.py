176706fc8d5d7844a9e30952ca76c32cf138d982
leopardShark <lamicccc@gmail.com>
2021-08-06 15:09:40 +0200

    include GMM for clustering

--- copyvae/clustering.py
+++ copyvae/clustering.py
@@ -1,10 +1,39 @@
 #! /usr/bin/env python3
 
-#import numpy as np
-from sklearn.cluster import KMeans
+import numpy as np
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.mixture import GaussianMixture
 
-def find_clones(data, n_clones=2):
-    """ Clustering method for finding clones """
+
+def find_clones_gmm(latent_data, expression_data, n_clones=2):
+    """ GMM find tumour clone
+
+    Args:
+        latent_data: latent feature array (cell x latent dim)
+        expression_data: copy number array (cell x gene or cell x bin)
+        n_clones: number of clones
+    Returns:
+        mask: mask for tumour cells
+    """
+
+    # fit GMM
+    gmm = GaussianMixture(n_components=n_clones, n_init=5, max_iter=100000)
+    gmm.fit(latent_data)
+    pred_label = gmm.predict(latent_data)
+
+    # define tumour clones
+    mask = (pred_label).astype(bool)
+    clone_masked = expression_data[mask]
+    clone_unmasked = expression_data[~mask]
+    if clone_masked.mean(axis=1).std() > clone_unmasked.mean(axis=1).std():
+        return mask
+    else:
+        mask = (1 - pred_label).astype(bool)
+        return mask
+
+
+def find_clones_kmeans(data, n_clones=2):
+    """ kmeans clustering method for finding clones """
 
     kmeans = KMeans(n_clusters=n_clones, random_state=0).fit(data)
     prediction = kmeans.labels_
@@ -12,8 +41,10 @@ def find_clones(data, n_clones=2):
     return prediction
 
 
-#d = np.load('./data/latent.npy')
-#pred = find_clones(d)
-#bincp_array = np.load('median_cp.npy')
-#mask = (1-pred).astype(bool)
-#cells = bincp_array[mask]
+def find_clones_dbscan(data, min_members=1):
+    """ DBSCAN clustering """
+
+    clustering = DBSCAN(min_samples=min_members).fit(data)
+    prediction = clustering.labels_
+
+    return prediction
--- copyvae/pipeline.py
+++ copyvae/pipeline.py
@@ -7,9 +7,10 @@ import tensorflow as tf
 from copyvae.binning import bin_genes_umi
 from copyvae.preprocess import annotate_data
 from copyvae.vae import CopyVAE, train_vae
-from copyvae.clustering import find_clones
+from copyvae.clustering import find_clones_gmm
 from copyvae.segmentation import bin_to_segment
 from copyvae.cell_tools import Clone
+#from copyvae.graphics import draw_umap, draw_heatmap, plot_breakpoints
 
 
 def run_pipeline(umi_counts):
@@ -18,9 +19,10 @@ def run_pipeline(umi_counts):
     Args:
         umi_counts: umi count text file
     Params:
-        bin_size: size of bin
-        intermediate_dim: intermediate dimension for vae
-        latent_dim: latent dimension for vae
+        max_cp: maximum copy number
+        bin_size: number of genes per bin
+        intermediate_dim: number of intermediate dimensions for vae
+        latent_dim: number of latent dimensions for vae
         batch_size: batch size for training
         epochs = number of epochs training
     """
@@ -30,14 +32,14 @@ def run_pipeline(umi_counts):
     intermediate_dim = 128
     latent_dim = 10
     batch_size = 128
-    epochs = 300
+    epochs = 250
 
-    ## assign genes to bins
+    # assign genes to bins
     binned_genes, chroms = bin_genes_umi(umi_counts, bin_size)
     adata = annotate_data(binned_genes)
     x_train = adata.X
 
-    ## train model
+    # train model
     model = CopyVAE(x_train.shape[-1],
                     intermediate_dim,
                     latent_dim,
@@ -45,54 +47,53 @@ def run_pipeline(umi_counts):
                     max_cp=max_cp)
     copy_vae = train_vae(model, x_train, batch_size, epochs)
 
-    ## get copy number and latent output
+    # get copy number and latent output
     z_mean, _, z = copy_vae.encoder.predict(adata.X)
     reconstruction, gene_cn, _ = copy_vae.decoder(z)
 
     adata.obsm['latent'] = z_mean
     #draw_umap(adata, 'latent', '_latent')
-    #draw_heatmap(gene_cn,'gene_copies')
+    # draw_heatmap(gene_cn,'gene_copies')
     with open('copy.npy', 'wb') as f:
         np.save(f, gene_cn)
 
-    ## compute bin copy number
+    # compute bin copy number
     gn = x_train.shape[1]
-    bin_number = gn//bin_size
+    bin_number = gn // bin_size
     tmp_arr = np.split(gene_cn, bin_number, axis=1)
     tmp_arr = np.stack(tmp_arr, axis=1)
     bin_cn = np.median(tmp_arr, axis=2)
-    #draw_heatmap(bin_cn,'bin_copies')
+    # draw_heatmap(bin_cn,'bin_copies')
     with open('median_cp.npy', 'wb') as f:
         np.save(f, bin_cn)
 
-    ## seperate normal cells
-    pred_label = find_clones(z_mean)
-    mask = (1-pred_label).astype(bool)
-    cells = bin_cn[mask]
+    # seperate tumour cells from normal
+    tumour_mask = find_clones_gmm(z_mean, adata.X)
+    cells = bin_cn[tumour_mask]
 
     clone_size = np.shape(cells)[0]
     t_clone = Clone(1,
                     clone_size,
                     bin_size,
-                    cell_gene_cn = gene_cn[mask],
-                    cell_bin_cn = bin_cn[mask],
-                    chrom_bound = chroms
+                    cell_gene_cn=gene_cn[tumour_mask],
+                    cell_bin_cn=bin_cn[tumour_mask],
+                    chrom_bound=chroms
                     )
-    
-    ## call clone breakpoints
+
+    # call clone breakpoints
     t_clone.call_breakpoints()
-    print(t_clone.breakpoints)
+    #print(t_clone.breakpoints)
     #cp_arr = np.mean(cells, axis=0)
     #plot_breakpoints(cp_arr, t_clone.breakpoints, 'bp_plot')
 
-    ## generate clone profile
+    # generate clone profile
     t_clone.generate_profile()
     clone_seg = t_clone.segment_cn
-    print(clone_seg)
-    #with open('clone.npy', 'wb') as f:
-    #   np.save(f, clone_seg)
+    #print(clone_seg)
+    with open('clone.npy', 'wb') as f:
+        np.save(f, clone_seg)
 
-    ## generate consensus segment profile
+    # generate consensus segment profile
     bp_arr = t_clone.breakpoints
     seg_profile = bin_to_segment(bin_cn, bp_arr)
     with open('segments.npy', 'wb') as f:
@@ -105,7 +106,7 @@ def run_pipeline(umi_counts):
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--input', required=True, help="input UMI")
+    parser.add_argument('input', type=argparse.FileType('r'), help="input UMI")
     parser.add_argument('-g', '--gpu', type=int, help="GPU id")
 
     args = parser.parse_args()
@@ -115,11 +116,10 @@ def main():
         dvc = '/device:GPU:{}'.format(args.gpu)
     else:
         dvc = '/device:GPU:0'
-    
+
     with tf.device(dvc):
         run_pipeline(file)
 
-    
 
 if __name__ == "__main__":
     main()
