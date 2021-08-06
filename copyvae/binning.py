1ea844a7e404cb519769afdc36647aa677fcd2f7
leopardShark <lamicccc@gmail.com>
2021-08-06 15:01:24 +0200

    change abs directory to gene map

--- copyvae/binning.py
+++ copyvae/binning.py
@@ -2,9 +2,13 @@
 
 import numpy as np
 import pandas as pd
+from os import path
+
+local_directory = path.dirname(path.abspath(__file__))
+gene_map = path.join(local_directory, '../data/mart_export.txt')
 
 
-def bin_genes_umi(umi_counts, bin_size, gene_metadata='../data/mart_export.txt'):
+def bin_genes_umi(umi_counts, bin_size, gene_metadata=gene_map):
     """ Gene binning for UMI counts
 
     Args:
@@ -18,56 +22,56 @@ def bin_genes_umi(umi_counts, bin_size, gene_metadata='../data/mart_export.txt')
     """
 
     umis = pd.read_csv(umi_counts, sep='\t', low_memory=False).reset_index()
-    labels = umis.iloc[1,:]
+    labels = umis.iloc[1, :]
     gene_info = pd.read_csv(gene_metadata, sep='\t')
-    gene_info.rename(columns={'Chromosome/scaffold name':'chr'}, inplace=True)
+    gene_info.rename(columns={'Chromosome/scaffold name': 'chr'}, inplace=True)
 
-    ## remove cell cycle genes
+    # remove cell cycle genes
     umis = umis[~umis['index'].str.contains("HLA")]
-    ## gene name mapping
+    # gene name mapping
     gene_merge = pd.merge(
-                            umis, gene_info, right_on=['Gene name'],
-                            left_on=['index'], how='inner'
-                            )
-    gene_merge.loc[gene_merge.chr=='X', 'chr'] = '23'
+        umis, gene_info, right_on=['Gene name'],
+        left_on=['index'], how='inner'
+    )
+    gene_merge.loc[gene_merge.chr == 'X', 'chr'] = '23'
     coding_gene = gene_merge[
-                                gene_merge['chr'].isin(
-                                                    np.arange(1,24).astype(str)
-                                                            )
-                                ]
+        gene_merge['chr'].isin(
+            np.arange(1, 24).astype(str)
+        )
+    ]
     coding_gene = coding_gene.copy()
-    coding_gene.loc[:,'chr'] = coding_gene.chr.astype(int)
-    sorted_gene = coding_gene.sort_values(by=['chr','Gene start (bp)'])
+    coding_gene.loc[:, 'chr'] = coding_gene.chr.astype(int)
+    sorted_gene = coding_gene.sort_values(by=['chr', 'Gene start (bp)'])
     sorted_gene.to_csv('sorted_genes.csv', sep='\t')
 
-    ## remove non-expressed genens
+    # remove non-expressed genes
     ch = sorted_gene.iloc[:, 1:-6].astype('int32')
     nonz = np.count_nonzero(ch, axis=1)
     sorted_gene['expressed'] = nonz
     expressed_gene = sorted_gene[sorted_gene['expressed'] > 0]
 
-    ## bin and remove exceeded genes
+    # bin and remove exceeded genes
     n_exceeded = expressed_gene.chr.value_counts() % bin_size
     for chrom in n_exceeded.index:
         n = n_exceeded[chrom]
         ind = expressed_gene[
-                                expressed_gene['chr']==chrom
-                                ].sort_values(by=['expressed','Gene start (bp)']
-                                                )[:n].index
+            expressed_gene['chr'] == chrom
+        ].sort_values(by=['expressed', 'Gene start (bp)']
+                      )[:n].index
         expressed_gene = expressed_gene.drop(index=ind)
-    
-    ## extract chromosome boundary
+
+    # extract chromosome boundary
     bin_number = expressed_gene.chr.value_counts() // bin_size
     chrom_bound = bin_number.sort_index().cumsum()
     chrom_list = [(0, chrom_bound[1])]
     for i in range(2, 24):
-        start_p = chrom_bound[i-1]
+        start_p = chrom_bound[i - 1]
         end_p = chrom_bound[i]
         chrom_list.append((start_p, end_p))
 
-    ## clean and add labels
+    # clean and add labels
     expressed_gene.drop(columns=expressed_gene.columns[-7:],
-                                                        axis=1, inplace=True)
+                        axis=1, inplace=True)
     expressed_gene = pd.concat([expressed_gene.T, labels], axis=1)
     expressed_gene.rename(columns=expressed_gene.iloc[0], inplace=True)
     expressed_gene.drop(expressed_gene.index[0], inplace=True)
