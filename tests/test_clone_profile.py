ed31e5e4d11c49844fd67fb4caafd5b01c57a82d
leopardShark <lamicccc@gmail.com>
2021-10-01 13:08:43 +0200

    Update test_clone_profile.py

--- tests/test_clone_profile.py
+++ tests/test_clone_profile.py
@@ -1,32 +1,10 @@
 #! /usr/bin/env python3
 
+import argparse
 import pandas as pd
 import numpy as np
-
-CHR_BASE_PAIRS = [
-    248956422,
-    242193529,
-    198295559,
-    190214555,
-    181538259,
-    170805979,
-    159345973,
-    145138636,
-    138394717,
-    133797422,
-    135086622,
-    133275309,
-    114364328,
-    107043718,
-    101991189,
-    90338345,
-    83257441,
-    80373285,
-    58617616,
-    64444167,
-    46709983,
-    50818468,
-    156040895]
+import matplotlib.pylab as plt
+from copyvae.binning import CHR_BASE_PAIRS
 
 
 def construct_position_df(base_per_region=180000):
@@ -56,3 +34,63 @@ def construct_position_df(base_per_region=180000):
     df = pd.DataFrame(data=narr, columns=['chrom', 'chrompos', 'abspos'])
     #df = df.iloc[5:].reset_index(drop=True)
     return df
+
+
+def test_dcis_inference(clone_profile, abs_position, gt_profile):
+    """ Test DCIS1 data inference
+
+    Args:
+        clone_profile: inferred clone profile
+        abs_position: absolute gene position in the genome
+        gt_profile: ground truth clone profile
+    Returns:
+        distance: Euclidean distance
+    """
+
+    # process groud truth
+    gt = pd.read_csv(gt_profile, sep='\t')
+    gt['abspos'] = gt['abspos'].astype('int')
+
+    #gt['copy'] = round(2**gt['med.DNA']*2)
+    gt['copy'] = 2**gt['med.DNA'] * 2
+
+    # estimate copy of positions in ground truth
+    clone = {'abspos': abs_position, 'cp_inf': clone_profile}
+    df = pd.DataFrame(data=clone)
+    compdf = pd.merge_asof(
+        gt.sort_values('abspos'),
+        df.sort_values('abspos'),
+        on='abspos')
+
+    distance = np.linalg.norm(compdf['copy'].values - compdf['cp_inf'].values)
+
+    compdf.loc[compdf['copy'] > 6, 'copy'] = 6.0
+    gtcp = compdf['copy'].values
+    infcp = compdf['cp_inf'].values
+    plt.figure(figsize=(17, 5), dpi=120)
+    plt.plot(infcp)
+    plt.plot(gtcp)
+    plt.savefig('dcis1.png')
+
+    return distance
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help="clone profile")
+    parser.add_argument('-a', '--abs', help="absolute position")
+    parser.add_argument('-gt', '--gt', help="ground truth")
+
+    args = parser.parse_args()
+    clone_profile = np.load(args.input)
+    abs_pos = np.load(args.abs)
+    gt_profile = args.gt
+
+    dis = test_dcis_inference(clone_profile, abs_pos, gt_profile)
+    print('Euclidean:')
+    print(dis)
+
+
+if __name__ == "__main__":
+    main()
